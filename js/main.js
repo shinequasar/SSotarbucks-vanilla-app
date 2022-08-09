4be6d422161759b507b2d099138869d045117ec2
shinequasar <shinequasar@gmail.com>
2022-08-09 16:12:57 +0900

    [feat] 프로모션 이미지 슬라이드 구현

--- js/main.js
+++ js/main.js
@@ -49,3 +49,28 @@ fadeEls.forEach(function (fadeEl,index){
       opacity:1
    });
 });
+
+
+// new Swiper(선택자, 옵션)
+new Swiper('.notice-line .swiper', {
+   direction: 'vertical',
+   autoplay : true,
+   loop: true
+});
+new Swiper('.promotion .swiper',{
+   slidesPerView:3, //한번에 보여줄 슬라이드 개수
+   spaceBetween: 10, // 슬라이드 사이 여백
+   centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+   loop: true ,
+   autoplay:{
+      delay:4000 //4초
+   },
+   pagination:{
+      el: '.promotion .swiper-pagination', //페이지 번호요소 선택자
+      clickable:true,
+      navigation:{
+         prevEl:'.promotion .swiper-prev',
+         nextEl:'.promotion .swiper-next'
+      }
+   }
+})
